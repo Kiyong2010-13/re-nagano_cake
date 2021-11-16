3b936d64d470ce2b62833e9064c18ec9d4df7942
Kiyong2010-13 <k.shogo1994@gmail.com>
2021-11-16 06:37:06 +0000

    genre.comp

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -9,15 +9,29 @@ class Admin::GenresController < ApplicationController
     if @genre.save
       redirect_to admin_genres_path
     else
+      @genres = Genre.all
       render :index
     end
-    
+
   end
 
   def edit
+    @genre = Genre.find(params[:id])
   end
 
   def update
+    @genre = Genre.find(params[:id])
+    if @genre.update(genre_params)
+      redirect_to admin_genres_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @genre = Genre.find(params[:id])
+    @genre.destroy
+    redirect_to admin_genres_path
   end
 
   private
