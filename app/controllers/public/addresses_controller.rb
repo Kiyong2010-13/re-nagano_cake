7d2df4c2749881cc102a34947e1719b6bd578452
Kiyong2010-13 <k.shogo1994@gmail.com>
2021-12-20 12:00:46 +0000

    add1.1

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,2 +1,34 @@
 class Public::AddressesController < ApplicationController
+  def index
+    @address = Address.new
+    @addresses = current_customer.addresses
+  end
+
+  def edit
+  end
+
+  def create
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+    if @address.save
+      redirect_to addresses_path
+    else 
+      @address = Address.new
+      @addresses = current_customer.addresses
+      render :index
+    end
+    
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:name, :postal_code, :address)
+  end
 end
