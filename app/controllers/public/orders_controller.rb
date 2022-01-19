ddcd66e256f8a58873832465f239cca12256ef87
Kiyong2010-13 <k.shogo1994@gmail.com>
2022-01-19 09:50:17 +0000

    orders1.3

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -11,17 +11,17 @@ class Public::OrdersController < ApplicationController
     @order = Order.new(order_params)
     if params[:order][:address_option] == "0"
       @order.postal_code = current_customer.postal_code
-      @order.address = current_customer.address
-      @order.name = current_customer.full_name
+      @order.shipping_address = current_customer.address
+      @order.delivery_name = current_customer.full_name
     elsif params[:order][:address_option] == "1"
       ship = Address.find(params[:order][:address_id])
       @order.postal_code = ship.postal_code
-      @order.address = ship.address
-      @order.name = ship.name
+      @order.shipping_address = ship.address
+      @order.delivery_name = ship.name
     elsif params[:order][:address_option] = "2"
       @order.postal_code = params[:order][:postal_code]
-      @order.address = params[:order][:address]
-      @order.name = params[:order][:name]
+      @order.shipping_address = params[:order][:address]
+      @order.delivery_name = params[:order][:name]
     else
           render 'new'
     end
@@ -42,6 +42,6 @@ class Public::OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order).permit(:shipping_cost, :payment_method, :name, :address, :postal_code, :customer_id, :total_payment, :status)
+    params.require(:order).permit(:shipping_cost, :payment_method, :delivery_name, :shipping_address, :postal_code, :customer_id, :total_payment, :status)
   end
 end
