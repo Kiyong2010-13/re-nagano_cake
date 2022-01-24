73ee185944da9ac07cb88b9873d92033cb5bcd6d
Kiyong2010-13 <k.shogo1994@gmail.com>
2022-01-24 02:38:45 +0000

    orders1.5

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -31,7 +31,22 @@ class Public::OrdersController < ApplicationController
   end
 
   def create
-  end
+    @order = Order.new(order_params)
+    @order.customer_id = current_customer.id
+    @order.save
+    
+    current_customer.cart_items.each do |cart_item|
+      @order_detail = OrderDetail.new
+      @order_detail.item_id = cart_item.item_id
+      @order_detail.amount = cart_item.amount
+      @order_detail.purchase_price = cart_item.item.add_tax_price*cart_item.amount
+      @order_detail.order_id = @order.id
+      @order_detail.save
+    end
+    
+    current_customer.cart_items.destroy_all
+    redirect_to complete_orders_path
+  end 
 
   def index
   end
